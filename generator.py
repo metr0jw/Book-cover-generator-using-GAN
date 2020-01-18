520ccf11828f11a1ded0ba98c9ce61fd4e0c8f5b
metr0jw <webmaster@kw.ac.kr>
2020-01-19 02:50:32 +0900

    First commit

---
+++ generator.py
@@ -0,0 +1,13 @@
+class Generator:
+
+        def __init__(self):
+            self.initVariable = 1
+
+        def lossFunction(self):
+            return
+
+        def buildModel(self):
+            return
+
+        def trainModel(self, inputX, inputY):
+            return
